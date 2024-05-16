1f43950bd07730c23ecb7cbb415494caf923a107
Afasarya <coderea9@gmail.com>
2024-05-16 15:34:07 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -0,0 +1,12 @@
+package com.your.myapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Payment.kt
@@ -0,0 +1,12 @@
+package com.your.myapplication
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class Payment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+    }
+}
